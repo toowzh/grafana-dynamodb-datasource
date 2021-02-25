9ef47e4b7f7d15c806a61eb6ee168e16189afa49
Yoshio Miyamae <miyamae0@tlv.co.jp>
2021-02-25 13:11:48 +0900

    change data retrieving method

---
+++ sign.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npx grafana-toolkit plugin:sign --rootUrls https://pec.tts-miop.net
--- src/DataSource.ts
+++ src/DataSource.ts
@@ -2,7 +2,6 @@ import defaults from 'lodash/defaults';
 
 import {
   DataQueryRequest,
-  DataQueryResponse,
   DataSourceApi,
   DataSourceInstanceSettings,
   MutableDataFrame,
@@ -33,7 +32,7 @@ export class DataSource extends DataSourceApi<DynamoDBQuery, DynamoDBOptions> {
     this.dynamoDB = new AWS.DynamoDB();
   }
 
-  async query(options: DataQueryRequest<DynamoDBQuery>): Promise<DataQueryResponse> {
+  async query(options: DataQueryRequest<DynamoDBQuery>) {
     const { range } = options;
     const from = range!.from.valueOf();
     const to = range!.to.valueOf();
@@ -46,63 +45,93 @@ export class DataSource extends DataSourceApi<DynamoDBQuery, DynamoDBOptions> {
         ':from': from / 1000,
         ':to': to / 1000,
       });
+      console.log([query.timeField, ...query.valueFields].join(', '));
       const queryInput: AWS.DynamoDB.QueryInput = {
         TableName: query.tableName,
         KeyConditionExpression: `${query.keyConditionExpression} AND ${query.timeField} BETWEEN :from AND :to`,
+        ProjectionExpression: [query.timeField, ...query.valueFields].join(', '),
         ExpressionAttributeValues: expressionAttributeValues,
       };
-      return this.dynamoDB
-        .query(queryInput)
-        .promise()
-        .then((data) => {
-          if (data.Items && data.Items.length >= 1) {
-            const items = data.Items.map((x) => AWS.DynamoDB.Converter.unmarshall(x));
-            let timeList = items.map((x) => new Date(Number(x[query.timeField]) * 1000));
-            let temp: { [key: string]: any[] } = {};
-            items.forEach((x) => {
-              Object.keys(x).forEach((k) => {
-                temp[k] = [];
-              });
-            });
-            items.forEach((x) => {
-              const keys = Object.keys(x);
-              query.valueFields
-                .filter((k) => keys.indexOf(k) === -1)
-                .forEach((k) => {
-                  temp[k].push(null);
-                });
-              keys.forEach((k) => {
-                temp[k].push(x[k]);
-              });
-            });
-            const values = Object.keys(temp)
-              .filter((k) => query.valueFields.indexOf(k) !== -1)
-              .map((k) => ({
-                name: k,
-                values: temp[k],
-                type: FieldType.number,
-              }));
-            const frame = new MutableDataFrame({
-              refId: query.refId,
-              fields: [
-                {
-                  name: 'Time',
-                  values: timeList,
-                  type: FieldType.time,
-                },
-                ...values,
-              ],
-              length: timeList.length,
-            });
 
-            return frame;
-          }
-          return null;
-        })
-        .catch((err) => console.log(err));
+      const results = this.doQuery(queryInput);
+      return this.processData(results, query);
     });
 
     return Promise.all(promises).then((data) => ({ data }));
+  }
+
+  async doQuery(queryInput: AWS.DynamoDB.QueryInput) {
+    let _queryInput = { ...queryInput };
+    let response = await this.dynamoDB.query(_queryInput).promise();
+    let temp = response.Items;
+    console.log(temp);
+    if (!temp) {
+      return null;
+    }
+    let results = temp.map((x) => AWS.DynamoDB.Converter.unmarshall(x));
+
+    while (response.LastEvaluatedKey) {
+      console.log(response.LastEvaluatedKey);
+      _queryInput.ExclusiveStartKey = response.LastEvaluatedKey;
+      response = await this.dynamoDB.query(_queryInput).promise();
+      if (!response.Items) {
+        break;
+      }
+      results = results.concat(response.Items.map((x) => AWS.DynamoDB.Converter.unmarshall(x)));
+      console.log(response.Items);
+    }
+    results = results.sort((a, b) => a['date_time'] - b['date_time']);
+    return results;
+  }
+
+  async processData(
+    results: Promise<Array<{ [key: string]: any }> | null>,
+    query: Partial<DynamoDBQuery> & DynamoDBQuery
+  ): Promise<MutableDataFrame<any> | null> {
+    const data = await results;
+    console.log(data);
+    if (data && data.length >= 1) {
+      let timeList = data.map((x_1) => new Date(Number(x_1[query.timeField]) * 1000));
+      let temp: { [key: string]: any[] } = {};
+      data.forEach((x_2) => {
+        Object.keys(x_2).forEach((k) => {
+          temp[k] = [];
+        });
+      });
+      data.forEach((x_3) => {
+        const keys = Object.keys(x_3);
+        query.valueFields
+          .filter((k_1) => keys.indexOf(k_1) === -1)
+          .forEach((k_2) => {
+            temp[k_2].push(null);
+          });
+        keys.forEach((k_3) => {
+          temp[k_3].push(x_3[k_3]);
+        });
+      });
+      const values = Object.keys(temp)
+        .filter((k_4) => query.valueFields.indexOf(k_4) !== -1)
+        .map((k_5) => ({
+          name: k_5,
+          values: temp[k_5],
+          type: FieldType.number,
+        }));
+      const frame = new MutableDataFrame({
+        refId: query.refId,
+        fields: [
+          {
+            name: 'Time',
+            values: timeList,
+            type: FieldType.time,
+          },
+          ...values,
+        ],
+        length: timeList.length,
+      });
+
+      return frame;
+    }
+    return null;
   }
 
   async testDatasource() {
